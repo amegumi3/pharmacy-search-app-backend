89970d59fa1921079d023eef2710be730baeb439
Megumi <megumi@Megumi-MBP.local>
2023-02-15 19:35:56 +0900

    [fix] 詳細ページのルーティングを数値のみ受け取るよう修正
    ・詳細ページでは、paramsでid受け取っていますが、idは数字しか受け取ってほしくないため、数字のみ受け取るよう変更しました。
    ・pahrmaciesコントローラーでは、updateやdestroyなどを使用していないため、onlyでアクションを制限しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :pharmacies do
-        collection do
-          post :import
-          post :report_import
-        end
-      end
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
       }
       namespace :auth do
         resources :sessions, only: [:index]
+      end
+      # resources :pharmacies  do としていた部分を変更
+      resources :pharmacies, only: [:index, :show], constraints: { id:  /\d+/ } do
+        collection do
+          post :import
+          post :report_import
+        end
       end
     end
   end
